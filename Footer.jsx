b52aab062362e92d111ac64c525af7d579453ceb
Garima2016 <72975570+Garima2016@users.noreply.github.com>
2021-06-06 21:43:55 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 function Footer() {
+
+  var currYear = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright @ 2021</p>
+      <p>Copyright @ {currYear}</p>
     </footer>
   );
 }
 
 export default Footer;
+
